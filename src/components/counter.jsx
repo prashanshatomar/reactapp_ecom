9aee733829192b6b7c2622ed07e6ccddbbe1d888
Prashansha Tomar <prashansha.tomar@esmart.systems>
2019-07-31 18:28:40 +0530

    Create react component and import and render to reactDOM

---
+++ src/components/counter.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    // state = {}
+    render() { 
+        return <h1>Hello World</h1>;
+    }
+}
+ 
+export default Counter;
+
+// can ue also this export syntax, direct export the class
+
+// export default class Counter extends Component {
+//     // state = {}
+//     render() { 
+//         return <h1>Hello World</h1>;
+//     }
+// }
