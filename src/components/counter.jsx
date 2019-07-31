892873e1c729b3423468e4078dc5aa1b7d7ccf90
Prashansha Tomar <prashansha.tomar@esmart.systems>
2019-07-31 18:37:58 +0530

    rap element in single parent example single div

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Counter extends Component {
-    // state = {}
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  // state = {}
+  render() {
+    // return <h1>Hello World</h1>;
+    //return <h1>Hello World</h1><button>Increment</button>;
+    //this will not work because babble not know how to deal with long multiple html element,
+    //therefore the sollution is to wrap the element in single parent element
+
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    ); //== React.createElement('div)
+  }
 }
- 
+
 export default Counter;
 
 // can ue also this export syntax, direct export the class
 
 // export default class Counter extends Component {
 //     // state = {}
-//     render() { 
+//     render() {
 //         return <h1>Hello World</h1>;
 //     }
 // }
